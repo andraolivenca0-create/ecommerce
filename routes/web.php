17a9ffedc3c2fb2b0e7f51e97228db2d87286980
jahranarshifadilah-cpu <jahranarshifadilah@gmail.com>
2025-12-16 11:45:34 +0700

    database desain dan migration

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/tentang', function () {
     return view('tentang');
 
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
